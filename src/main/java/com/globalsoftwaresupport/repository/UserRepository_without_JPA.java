0ba50a8e7ff19df3c6b26235dd7473d72298bf77
jackrichter <jackrichter1@gmail.com>
2023-08-21 07:34:10 +0200

    Keeping the previous UserRepository for future reference.

---
+++ src/main/java/com/globalsoftwaresupport/repository/UserRepository_without_JPA.java
@@ -0,0 +1,36 @@
+package com.globalsoftwaresupport.repository;
+
+import com.globalsoftwaresupport.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserRepository_without_JPA {
+    private List<User> users;
+
+    public UserRepository_without_JPA() {
+        this.users = new ArrayList<>();
+        users.add(new User("Adam", "Spacey", "adam@gmail.com"));
+        users.add(new User("Dan", "Smith", "dan@gmail.com"));
+        users.add(new User("Ana", "Heck", "ana@gmail.com"));
+    }
+
+    public List<User> getUsers() {
+        return this.users;
+    }
+
+    public void save(User user) {
+//        user.setId((long)users.size());
+        this.users.add(user);
+    }
+
+    public void delete(Integer id) {
+        this.users.remove(id);
+    }
+
+    public void update(String userId, User user) {
+        this.users.set(Integer.valueOf(userId), user);
+    }
+}
